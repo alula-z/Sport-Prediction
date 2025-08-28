08d4fbe3841179b183ceb3bab805f72ecbd9c57d
alula-z <azeruesenay14@gmail.com>
2025-08-28 17:50:29 -0400

    initial commit

---
+++ SportPrediction/SportPredictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportPredictionApp.swift
+//  SportPrediction
+//
+//  Created by Alula Zeruesenay on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SportPredictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SportPredictionTests/SportPredictionTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportPredictionTests.swift
+//  SportPredictionTests
+//
+//  Created by Alula Zeruesenay on 8/28/25.
+//
+
+import Testing
+@testable import SportPrediction
+
+struct SportPredictionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
